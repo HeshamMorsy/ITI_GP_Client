5a25a9942cb8fcb8bc0e5e92b33991aced535d08
hazem ashraf <hazem_ashraf126@yahoo.com>
2018-05-25 01:42:54 +0200

    signup

--- app/src/main/java/app/iti/client/iti_gp_client/screens/login/LoginActivity.kt
+++ app/src/main/java/app/iti/client/iti_gp_client/screens/login/LoginActivity.kt
@@ -1,13 +1,18 @@
 package app.iti.client.iti_gp_client.screens.login
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import app.iti.client.iti_gp_client.R
+import app.iti.client.iti_gp_client.screens.signup.SignUpActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, SignUpActivity::class.java)
+        // start your next activity
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/app/iti/client/iti_gp_client/screens/signup/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package app.iti.client.iti_gp_client.screens.signup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import app.iti.client.iti_gp_client.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
