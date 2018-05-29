e68fa709fac268c8f39a70f87ea772cedec099ea
hazem ashraf <hazem_ashraf126@yahoo.com>
2018-05-29 14:52:19 +0200

    signup

--- app/src/main/java/app/iti/client/iti_gp_client/screens/login/LoginActivity.kt
+++ app/src/main/java/app/iti/client/iti_gp_client/screens/login/LoginActivity.kt
@@ -3,16 +3,23 @@ package app.iti.client.iti_gp_client.screens.login
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import app.iti.client.iti_gp_client.R
 import app.iti.client.iti_gp_client.screens.signup.SignUpActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val intent = Intent(this, SignUpActivity::class.java)
-        // start your next activity
-        startActivity(intent)
+        setContentView(R.layout.activity_login)
+
+
+        gotoSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/app/iti/client/iti_gp_client/screens/signup/SignUpActivity.kt
+++ app/src/main/java/app/iti/client/iti_gp_client/screens/signup/SignUpActivity.kt
@@ -1,13 +1,22 @@
 package app.iti.client.iti_gp_client.screens.signup
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import app.iti.client.iti_gp_client.R
+import app.iti.client.iti_gp_client.screens.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        loginLabel.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+            finish()
+        }
     }
 }
