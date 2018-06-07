3f01be32e801de73539cdcc03620c00c3d343c29
hazem ashraf <hazem_ashraf126@yahoo.com>
2018-06-08 00:25:59 +0200

    request screen without spinner

---
+++ app/src/main/java/app/iti/client/iti_gp_client/screens/dropOffLocation/DropOffActivity.kt
@@ -0,0 +1,13 @@
+package app.iti.client.iti_gp_client.screens.dropOffLocation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import app.iti.client.iti_gp_client.R
+
+class DropOffActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drop_off)
+    }
+}
