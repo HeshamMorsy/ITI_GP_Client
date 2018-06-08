db7afc2086a22fc234f9e5821fd4d3a0137fb236
HeshamMorsy <hesham0hero@gmail.com>
2018-06-09 00:17:53 +0200

    login = error dialog+progress dialog

--- app/src/main/java/app/iti/client/iti_gp_client/screens/login/LoginActivity.kt
+++ app/src/main/java/app/iti/client/iti_gp_client/screens/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package app.iti.client.iti_gp_client.screens.login
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import app.iti.client.iti_gp_client.R
 import app.iti.client.iti_gp_client.screens.forgot_password.ForgotPasswordActivity
 import app.iti.client.iti_gp_client.screens.signup.SignUpActivity
@@ -18,6 +21,8 @@ import app.iti.client.iti_gp_client.screens.order_description.OrderActivity
 class LoginActivity : AppCompatActivity(), View, android.view.View.OnFocusChangeListener {
     // reference to presenter
     var mPresenter:Presenter?=null
+    // progress dialog
+    var dialoge: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -78,11 +83,29 @@ class LoginActivity : AppCompatActivity(), View, android.view.View.OnFocusChange
         }
     }
 
+
     override fun goToHomeScreen() {
         val intent = Intent(this, HomeActivity::class.java)
         // start home activity
         startActivity(intent)
         finish()
+    }
+
+    override fun startLoading(mes:String){
+        Log.i("response", "start loading function")
+        val builder = AlertDialog.Builder(this)
+        val dialougeView = layoutInflater.inflate(R.layout.progress_dialouge,null)
+        val message = dialougeView.findViewById<TextView>(R.id.loadingmessage)
+        message.text = mes
+        builder.setView(dialougeView)
+        builder.setCancelable(false)
+        dialoge = builder.create()
+        dialoge?.show()
+    }
+
+    override fun endLoading(){
+        Log.i("response", "end loading function")
+        dialoge?.dismiss()
     }
 
 }
